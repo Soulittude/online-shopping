47a85177f0d6dca7e4ad89b8bcb2ffca3dbed675
requizm <mehmet41650@gmail.com>
2021-05-12 23:35:44 +0300

    Otomatik data oluşuyor
    -Bundan sonra update-database komutunu girince, 3 adet platform ve firma da ekleniyor.

--- OnlineShopping/Models/ModelContext.cs
+++ OnlineShopping/Models/ModelContext.cs
@@ -11,8 +11,9 @@ namespace OnlineShopping.Models
     {
         public ModelContext(DbContextOptions<ModelContext> options) : base(options)
         {
-            Database.Migrate();
+            
         }
+
 
         public DbSet<Firma> Firmalar { get; set; } //CRUD operations
 
@@ -23,5 +24,22 @@ namespace OnlineShopping.Models
         public DbSet<Tag> Tagler { get; set; }
 
         public DbSet<Oyun> Oyunlar { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Platform>()
+                        .HasData(
+                         new Platform { Id = 1, Ad = "Windows" },
+                         new Platform { Id = 2, Ad = "Linux" },
+                         new Platform { Id = 3, Ad = "MacOS" });
+
+            modelBuilder.Entity<Firma>()
+                       .HasData(
+                        new Firma { Id = 1, Ad = "Ubisoft", Ulke = "Azerbaycan", Website = "www.cum.zone" },
+                        new Firma { Id = 2, Ad = "Erzurum Game Studios", Ulke = "Türkiye", Website = "www.erzu.rum" },
+                        new Firma { Id = 3, Ad = "Rockstar", Ulke = "Erzincan", Website = "www.rock.star" });
+
+
+        }
     }
 }
