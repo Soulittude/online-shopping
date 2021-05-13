d8341854f74adfe64d61574b051361ad7bee85cd
Soulittude <greatmaotun@gmail.com>
2021-05-13 21:46:37 +0300

    Oyun Dilleri, Bug Fixes
    Hummalı bir çalışmanın ardından.

---
+++ OnlineShopping/Models/DTO/OyunDilDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopping.Models.DTO
+{
+    public class OyunDilDTO
+    {
+        public int OyunID { get; set; }
+
+        public int DilID { get; set; }
+    }
+}
